dac7ca72e21c406fbd6516a214f8204dd2698507
cameron559 <cameronmilsom7@gmail.com>
2025-11-14 17:05:23 +0000

    Initial files commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+const passwordLength = 15
+
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
+
+function generatePasswords() {
+    passwordOne.textContent = ""
+    passwordTwo.textContent = ""
+    for (let i = 0; i < passwordLength; i++) {
+        let randomIndex1 = Math.floor( Math.random() * characters.length)
+        let randomIndex2 = Math.floor( Math.random() * characters.length) 
+        
+        passwordOne.textContent += characters[randomIndex1]
+        passwordTwo.textContent += characters[randomIndex2] 
+    }
+}
+
+
+
